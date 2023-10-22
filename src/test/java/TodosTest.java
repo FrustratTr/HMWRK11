74ec426f2de0c78d33e9625562f1cb4f8eb6e2a7
Mark <mark.bogachev.00@mail.ru>
2023-10-22 18:45:35 +0300

    New test added

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -3,6 +3,23 @@ import org.junit.jupiter.api.Test;
 
 public class TodosTest {
     @Test
+    public void shouldSearchTasksByQueryWhenMultipleTasksMatch() {
+
+        SimpleTask simpleTask1 = new SimpleTask(1, "Buy groceries");
+        SimpleTask simpleTask2 = new SimpleTask(2, "Buy gifts");
+        SimpleTask simpleTask3 = new SimpleTask(3, "Buy books");
+
+        Todos todos = new Todos();
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+
+        // Тест: Находится несколько задач
+        Task[] groceryTasks = todos.search("Buy");
+        Task[] expected = { simpleTask1, simpleTask2, simpleTask3 };
+        Assertions.assertArrayEquals(expected, groceryTasks);
+    }
+    @Test
     public void shouldAddThreeTasksOfDifferentTypes() {
 
         SimpleTask simpleTask = new SimpleTask(1, "Buy groceries");
